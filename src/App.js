55c96e6c52a12bc35a778451bc7100870693bf3e
carolalbertini <carolallbertini@hotmail.com>
2023-06-07 00:11:04 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   const [input, setInput] = useState('');
   const [cep, setCep] = useState("");
 
-
   async function handleSearch(){
     
     if(input === ''){
@@ -40,7 +39,7 @@ function App() {
         onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className="buttonSearch" onClick={handleSearch}> 
+        <button className="buttonSearch" onClick={handleSearch} > 
           <FiSearch size={25} color='#FFF'/>
         </button>
       </div>
@@ -51,10 +50,9 @@ function App() {
         <main className='main'>
           <h2>CEP: {cep.cep}</h2>
 
-          <span>{cep.logradouro}</span>
-          <span>Complemento: {cep.complemento}</span>
-          <span>{cep.bairro}</span>
-          <span>{cep.localidade}</span>
+          <span>Logradouro: {cep.logradouro}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Cidade: {cep.localidade}</span>
         </main>
       )}
 
